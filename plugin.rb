77e99fedf3ca7bc6be9d2e11b8da3b30ab15846d
merefield <merefield@gmail.com>
2026-06-13 18:46:39 +0100

    CHORE: bump chat fork version to 0.5f

--- plugin.rb
+++ plugin.rb
@@ -3,7 +3,7 @@
 # name: chat
 # about: (merefield CUSTOMISED FORK) Adds chat functionality to your site so it can natively support both long-form and short-form communication needs of your online community
 # meta_topic_id: 230881
-# version: 0.4f
+# version: 0.5f
 # authors: Kane York, Mark VanLandingham, Martin Brennan, Joffrey Jaffeux
 # url: https://github.com/discourse/discourse/tree/main/plugins/chat
 # meta_topic_id: 230881
